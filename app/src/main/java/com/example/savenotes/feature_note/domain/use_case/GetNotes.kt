c54308fa300e57a97d086d3cd7ddf9c7d493c8d7
Alyasa62 <m.alyasa62@gmail.com>
2025-08-29 21:39:40 +0500

    created something

--- app/src/main/java/com/example/savenotes/feature_note/domain/use_case/GetNotes.kt
+++ app/src/main/java/com/example/savenotes/feature_note/domain/use_case/GetNotes.kt
@@ -2,12 +2,34 @@ package com.example.savenotes.feature_note.domain.use_case
 
 import com.example.savenotes.feature_note.domain.model.Note
 import com.example.savenotes.feature_note.domain.repository.NoteRepository
+import com.example.savenotes.feature_note.domain.util.NoteOrder
+import com.example.savenotes.feature_note.domain.util.OrderType
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
 class GetNotes(
     private val repository: NoteRepository
 ) {
-    operator fun invoke(): Flow<List<Note>> {
-        return repository.getNotes()
+    operator fun invoke(
+        noteOrder: NoteOrder = NoteOrder.Date(OrderType.Descending)
+    ): Flow<List<Note>> {
+        return repository.getNotes().map {
+            when(noteOrder.orderType) {
+                is OrderType.Ascending -> {
+                    when(noteOrder) {
+                        is NoteOrder.Title -> it.sortedBy { note -> note.title.lowercase() }
+                        is NoteOrder.Date -> it.sortedBy { note -> note.timestamp }
+                        is NoteOrder.Color -> it.sortedBy { note -> note.color }
+                    }
+                }
+                is OrderType.Descending -> {
+                    when(noteOrder) {
+                        is NoteOrder.Title -> it.sortedByDescending { note -> note.title.lowercase() }
+                        is NoteOrder.Date -> it.sortedByDescending { note -> note.timestamp }
+                        is NoteOrder.Color -> it.sortedByDescending { note -> note.color }
+                    }
+            }
+         }
+    }
     }
 }
