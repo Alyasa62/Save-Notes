d22127cab30610a305329570c6c33ac991e1824f
Alyasa62 <m.alyasa62@gmail.com>
2025-09-02 22:28:49 +0500

    added a file

---
+++ app/src/main/java/com/example/savenotes/feature_note/domain/use_case/RandomFile.kt
@@ -0,0 +1,19 @@
+package com.example.savenotes.feature_note.domain.use_case
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.wear.compose.material.Text
+
+@Composable
+fun Random(modifier: Modifier = Modifier) {
+    Column {
+        Text(
+            text = "Hello from Random File",
+            modifier = modifier
+                .fillMaxWidth()
+        )
+    }
+
+}
